0c718417d707782cfb6958cb05c4fa084f1e4f36
Akhil <akhil.madanan@nlindia.com>
2014-08-05 12:29:45 +0530

    asd

--- Notification.js
+++ Notification.js
@@ -53,70 +53,58 @@
 
 
 //Android push notification handler
-    function onNotificationGCM(e) {
-        $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+   function onNotificationGCM(e) {
+    $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
 
-        alert( e.regid);
-        
-        switch( e.event )
+    switch( e.event )
+    {
+    case 'registered':
+        if ( e.regid.length > 0 )
         {
-        case 'registered':
-            if ( e.regid.length > 0 )
-            {
-                $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
-                // Your GCM push server needs to know the regID before it can push to this device
-                // here is where you might want to send it the regID for later use.
-              alert( e.regid);
-                
-                var reqData ={"AppType":"2","DeviceId":""+e.regid+"","IPAddress":"","UserId":"0"};
-                
-               
-                ajaxcall("UpdateUserDetailsAndFetchDefaultCountry",reqData,IsDeviceRegResponseSuccess,errorfunction);
-               
-                
-            }
-        break;
+            $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+            // Your GCM push server needs to know the regID before it can push to this device
+            // here is where you might want to send it the regID for later use.
+            console.log("regID = " + e.regid);
+            alert(e.regid);
+        }
+    break;
 
-        case 'message':
-                 alert("notrification");
-            // if this flag is set, this notification happened while we were in the foreground.
-            // you might want to play a sound to get the user's attention, throw up a dialog, etc.
-            if ( e.foreground )
+    case 'message':
+        // if this flag is set, this notification happened while we were in the foreground.
+        // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+        if ( e.foreground )
+        {
+            $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+
+            // if the notification contains a soundname, play it.
+            var my_media = new Media("/android_asset/www/"+e.soundname);
+            my_media.play();
+        }
+        else
+        {  // otherwise we were launched because the user touched a notification in the notification tray.
+            if ( e.coldstart )
             {
-                $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
-                
-                alert("notrification");
-                // if the notification contains a soundname, play it.
-               // var my_media = new Media("/android_asset/www/"+e.soundname);
-               // my_media.play();
+                $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
             }
             else
-            {  // otherwise we were launched because the user touched a notification in the notification tray.
-              alert("notrification123456");
-                
-                if ( e.coldstart )
-                {
-                    $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
-                }
-                else
-                {
-                    $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
-                }
+            {
+                $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
             }
+        }
 
-            $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-            $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
-        break;
+        $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+        $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+    break;
 
-        case 'error':
-            $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
-        break;
+    case 'error':
+        $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+    break;
 
-        default:
-            $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
-        break;
-      }
-    }
+    default:
+        $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+    break;
+  }
+}
 
 
     function IsDeviceRegResponseSuccess(result)
@@ -154,8 +142,8 @@
         // here is where you might want to send it the token for later use.
         alert('device token = ' + result);
             
-            var reqData ={"AppType":"1","DeviceId":""+result+"","IPAddress":"","UserId":"0"};
-            ajaxcall("UpdateUserDetailsAndFetchDefaultCountry",reqData,IsDeviceRegResponseSuccess,errorfunction);
+//            var reqData ={"AppType":"1","DeviceId":""+result+"","IPAddress":"","UserId":"0"};
+//            ajaxcall("UpdateUserDetailsAndFetchDefaultCountry",reqData,IsDeviceRegResponseSuccess,errorfunction);
 //            
         }
     function onNotificationAPN (event)
