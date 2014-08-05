810c54e0b847669700d8d76cb79b8bc0740dadb8
Akhil <akhil.madanan@nlindia.com>
2014-08-05 11:17:01 +0530

    asd

--- Notification.js
+++ Notification.js
@@ -53,67 +53,30 @@
 
 
 //Android push notification handler
-    function onNotificationGCM(e) {
-        $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
-
+   onNotificationGCM: function(e) {
         switch( e.event )
         {
-        case 'registered':
-            if ( e.regid.length > 0 )
-            {
-                $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
-                // Your GCM push server needs to know the regID before it can push to this device
-                // here is where you might want to send it the regID for later use.
-              alert( e.regid);
-                
-                var reqData ={"AppType":"2","DeviceId":""+e.regid+"","IPAddress":"","UserId":"0"};
-                
-               
-                ajaxcall("UpdateUserDetailsAndFetchDefaultCountry",reqData,IsDeviceRegResponseSuccess,errorfunction);
-               
-                
-            }
-        break;
-
-        case 'message':
-                 alert("notrification");
-            // if this flag is set, this notification happened while we were in the foreground.
-            // you might want to play a sound to get the user's attention, throw up a dialog, etc.
-            if ( e.foreground )
-            {
-                $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
-                
-                alert("notrification");
-                // if the notification contains a soundname, play it.
-               // var my_media = new Media("/android_asset/www/"+e.soundname);
-               // my_media.play();
-            }
-            else
-            {  // otherwise we were launched because the user touched a notification in the notification tray.
-              alert("notrification123456");
-                
-                if ( e.coldstart )
+            case 'registered':
+                if ( e.regid.length > 0 )
                 {
-                    $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
                 }
-                else
-                {
-                    $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
-                }
-            }
-
-            $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-            $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
-        break;
-
-        case 'error':
-            $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
-        break;
-
-        default:
-            $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
-        break;
-      }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
     }
 
 
