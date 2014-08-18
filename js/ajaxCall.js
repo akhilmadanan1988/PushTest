2d79f2d4cb2a471b479881b49df8c7528ae1de62
Akhil <akhil.madanan@nlindia.com>
2014-08-18 13:28:29 +0530

    dsf

--- js/ajaxCall.js
+++ js/ajaxCall.js
@@ -1,9 +1,10 @@
-var string = device.name;
+
 
  document.addEventListener("deviceready", onDeviceReady, false);
 
  function onDeviceReady() {
      
+     alert(123);
      
   alert("device id"+device.uuid );   
      
